e6494a892c836949a28b34872212c7004096b179
TatyanaPonomareva <tanyfka2010p@gmail.com>
2024-11-19 16:44:25 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string=input()
+print(my_string)
+age=16
+print(my_string.upper())
+print(my_string.upper().lower())
+my_string = "Татьяна Пономарева"
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
